3c4b962fff255df8f775d45de82c147a3605fdba
Andrey Tondo <andreytondo0408@gmail.com>
2025-08-26 21:01:51 -0300

    initial commit

---
+++ dainf-lab-server/src/main/java/br/edu/utfpr/dainf/DainfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.dainf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainfApplication.class, args);
+	}
+
+}
---
+++ dainf-lab-server/src/test/java/br/edu/utfpr/dainf/DainfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.dainf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
