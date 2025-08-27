ec9e2bd8f8c7da23134a84c2d0dfde594d3c7234
Andrey Tondo <andreytondo0408@gmail.com>
2025-08-26 21:59:00 -0300

    base api project

---
+++ dainf-lab-server/src/main/java/br/edu/utfpr/dainf/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,55 @@
+package br.edu.utfpr.dainf.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public WarnMessage handlerSQLException(ConstraintViolationException exception, HttpServletRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", exception.getMessage());
+        return new WarnMessage(HttpStatus.BAD_REQUEST.value(), "Erro ao realizar o registro no banco de dados", request.getServletPath(), errors);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public WarnMessage handlerSQLException(DataIntegrityViolationException exception, HttpServletRequest request) {
+        return new WarnMessage(HttpStatus.BAD_REQUEST.value(), "Erro na integridade dos dados", request.getServletPath(), null);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public WarnMessage handlerValidationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        BindingResult result = exception.getBindingResult();
+
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for(FieldError fieldError: result.getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new WarnMessage(HttpStatus.BAD_REQUEST.value(), "Erro ao validar as informações!", request.getServletPath(), validationErrors);
+    }
+
+    @ExceptionHandler({WarnException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public WarnMessage handlerWarnException(WarnException exception, HttpServletRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", exception.getMessage());
+        return new WarnMessage(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), request.getServletPath(), errors);
+    }
+}
---
+++ dainf-lab-server/src/main/java/br/edu/utfpr/dainf/exception/WarnMessage.java
@@ -0,0 +1,25 @@
+package br.edu.utfpr.dainf.exception;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class WarnMessage {
+
+    private long timestamp = new Date().getTime();
+    private int status;
+    private String message;
+    private String url;
+    private Map<String, String> errors;
+
+    public WarnMessage(int status, String message, String url, Map<String, String> errors) {
+        this.status = status;
+        this.message = message;
+        this.url = url;
+        this.errors = errors;
+    }
+}
