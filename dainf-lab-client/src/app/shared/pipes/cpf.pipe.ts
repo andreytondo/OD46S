07c44b95842fb0e43eb89a542b3afbda5778e16b
Andrey Tondo <andreytondo0408@gmail.com>
2025-10-31 20:05:12 -0300

    feat: add pipes

---
+++ dainf-lab-client/src/app/shared/pipes/cpf.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cpf',
+  standalone: true
+})
+export class CpfPipe implements PipeTransform {
+  transform(value: string | number | null | undefined): string {
+    if (!value) return '';
+    const digits = value.toString().replace(/\D/g, '');
+    if (digits.length !== 11) return value.toString();
+
+    return digits.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  }
+}
---
+++ dainf-lab-client/src/app/shared/pipes/telefone.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'telefone'
+})
+export class TelefonePipe implements PipeTransform {
+  transform(value: string | number | null | undefined): string {
+    if (!value) return '';
+    const digits = value.toString().replace(/\D/g, '');
+
+    if (digits.length === 10) {
+      // (99) 9999-9999
+      return digits.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+    } else if (digits.length === 11) {
+      // (99) 9 9999-9999
+      return digits.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
+    }
+
+    return value.toString();
+  }
+}
