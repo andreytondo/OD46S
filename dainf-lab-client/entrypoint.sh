2f147d0b3c0214264a51b6e3a1d55ed45e2e94fd
Andrey Tondo <andreytondo0408@gmail.com>
2025-11-13 01:12:51 -0300

    feat: ci/cd pipeline

---
+++ dainf-lab-client/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+CONFIG_DIR="/usr/share/nginx/html/config"
+CONFIG_FILE="$CONFIG_DIR/config.json"
+TEMPLATE="/config.template.json"
+
+mkdir -p "$CONFIG_DIR"
+
+# Perform substitution
+echo "Generating runtime config from template..."
+envsubst < "$TEMPLATE" > "$CONFIG_FILE"
+
+echo "✅ Config generated at $CONFIG_FILE"
+exec nginx -g 'daemon off;'
